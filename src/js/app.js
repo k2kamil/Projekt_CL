4328d5bd5ebeae1e89b5efd7a38ba7437a970f2a
Kamil Cholys <kamilcholys@gmail.com>
2020-03-09 11:23:57 +0100

    Add HomePage

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,62 @@
-import $ from 'jquery';
+/* eslint-disable */
+//Można owinąc w domcontent loaded
+console.log('test');
 
-import './module/cat';
 
-window.$ = $;
-window.jQuery = $;
+//PONIŻEJ KOD DLA IMAGE SLIDER
+//Stałe
+let sliderImages = document.querySelectorAll('.slide');
+let arrowLeft = document.querySelector('#arrow-left');
+let arrowRight = document.querySelector('#arrow-right');
+let current = 0;
+
+//Clear all Images
+function reset() {
+    for(let i = 0; i < sliderImages.length; i++){
+        sliderImages[i].style.display = 'none';
+    }
+}
+
+//Init slider
+function startSlide () {
+    reset();
+    sliderImages[0].style.display = 'block';
+}
+
+// Show previous Image
+function slideLeft(){
+    reset();
+    sliderImages[current - 1].style.display = 'block';
+    current--;
+}
+
+//Show next
+function slideRight () {
+    reset();
+    sliderImages[current + 1].style.display = 'block';
+    current++;
+}
+
+
+// Left arrow click
+arrowLeft.addEventListener('click', function () {
+    if(current === 0) {
+        current = sliderImages.length;
+    }
+
+    slideLeft();
+});
+
+//Right arrow click
+arrowRight.addEventListener('click', function () {
+    if(current === sliderImages.length - 1) {
+        current = -1;
+    }
+
+    slideRight();
+});
+
+startSlide();
+
+//Hidden images
+
